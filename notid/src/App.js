0bb3c3f1325a70be1fc48c0efe88a1b0558a6ced
kle35 <kevinle717@gmail.com>
2022-11-29 23:26:07 -0500

    Update

---
+++ notid/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Sidebar from './Sidebar';
+import Main from './Main';
+
+function App() {
+  return (
+    <div className="App">
+      <Sidebar />
+      <Main />
+    </div>
+  );
+}
+
+export default App;
+
