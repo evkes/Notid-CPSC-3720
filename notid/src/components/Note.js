7d5d0d2597544ff20f6933c975ff01bec7d6ba6a
evkes <evkessler2002@gmail.com>
2022-12-08 12:46:51 -0500

    Added possible code

--- notid/src/components/Note.js
+++ notid/src/components/Note.js
@@ -1,7 +1,13 @@
-const Note = ({ id, tag, title, text, date, handleDeleteNote, handleOnClickNote}) => {
+const Note = ({ id, tag, title, text, date, handleDeleteNote}) => {
+	
+	const getNote = () => {
+		console.log(id);
+		return [id, tag, title, text, date];
+	}
+
 	return (
 		<div className='note'>
-			<div className="app-sidebar-note" onClick = {() => handleOnClickNote}>
+			<div className="app-sidebar-note" onClick = {() => getNote()}>
 					<div className = "tag-data">
 						<u>{tag}</u>
 					</div>
